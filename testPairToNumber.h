2118fd0166c4d0dc268b31761a4c3bee072f9b13
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-08-24 22:52:10 +0530

    Create testPairToNumber.h

---
+++ testPairToNumber.h
@@ -0,0 +1,16 @@
+#ifndef TESTPAIRTONUMBER_H
+#define TESTPAIRTONUMBER_H
+
+#include <iostream>
+#include<assert.h>
+
+class testPairToNumber
+{
+public:
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber) 
+};
+
+#endif
